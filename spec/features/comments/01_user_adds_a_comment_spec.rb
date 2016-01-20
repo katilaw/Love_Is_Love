16d0bdfa6be0f77b98a6304df65817fc28c82faf
Lawrence Kati <katilaw@gmail.com>
2016-01-19 19:16:13 -0500

    Started creating comment test, switching branches

---
+++ spec/features/comments/01_user_adds_a_comment_spec.rb
@@ -0,0 +1,12 @@
+
+feature 'let an authenticated user comment on a story', %{
+    As an authenticated user
+    I want to have access to the stories
+    So I can comment on them
+} do
+
+  # Acceptance:
+  # [ ] I can comment on a story
+  # [ ] A successfully comment should appear story's show page
+  # [ ] Most recent comments should appear first
+  # [ ] An unsuccessful comment should not appear on the stories show page
